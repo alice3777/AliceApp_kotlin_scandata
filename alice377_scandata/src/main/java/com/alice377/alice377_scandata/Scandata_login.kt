2a104cdf262f1021bc2b9396dc6ae48d9198dbc3
taipelsky <T170337@nanjuen.com.tw>
2019-05-28 08:29:58 +0800

    New module create and start.

---
+++ alice377_scandata/src/main/java/com/alice377/alice377_scandata/Scandata_login.kt
@@ -0,0 +1,12 @@
+package com.alice377.alice377_scandata
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Scandata_login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scandata_login)
+    }
+}
